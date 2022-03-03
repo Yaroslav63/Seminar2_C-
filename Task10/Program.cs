95d99dfd43c4d1bbbd270d81a252487e073e551d
Yaroslav63 <era6300@mail.ru>
2022-03-03 23:54:12 +0300

    add info

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 num = num / 10;
 int result = num % 10;
-Console.WriteLine(result);
+Console.WriteLine($"Вторая цифра числа - {result}");
