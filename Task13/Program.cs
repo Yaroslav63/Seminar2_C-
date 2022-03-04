14df28e56551e9c1b735044928880bbd47769454
Yaroslav63 <era6300@mail.ru>
2022-03-04 21:28:21 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра - {(num / 100) % 10}");
+}
